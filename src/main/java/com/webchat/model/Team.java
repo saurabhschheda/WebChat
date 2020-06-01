4c7cc064f9b46a3809a5c0d90631bfc265131e0b
saurabhschheda <saurabh.s.chheda@gmail.com>
2020-06-01 22:29:41 +0530

    Code clean up

--- src/main/java/com/webchat/model/Team.java
+++ src/main/java/com/webchat/model/Team.java
@@ -1,6 +1,5 @@
 package com.webchat.model;
 
-import com.webchat.db.DBService;
 import com.webchat.db.impl.MariaDBClient;
 
 import java.io.IOException;
@@ -32,50 +31,6 @@ public class Team {
         return teamName;
     }
 
-
-
-//	private void getTeamID() throws SQLException {
-//		String query = "SELECT ID FROM Team WHERE Name = '" + teamName + "' AND Org_ID = '" + orgId + "';";
-//		PreparedStatement ps = con.prepareStatement(query);
-//		ResultSet rs = ps.executeQuery();
-//		rs.first();
-//		teamID = rs.getInt(1);
-//		rs.close();
-//		ps.close();
-//	}
-//
-//	private void getTeamName() throws SQLException {
-//		String query = "SELECT Name FROM Team WHERE ID = '" + teamID + "';";
-//		PreparedStatement ps = con.prepareStatement(query);
-//		ResultSet rs = ps.executeQuery();
-//		rs.first();
-//		teamName = rs.getString(1);
-//		rs.close();
-//		ps.close();
-//	}
-//
-//	private int getOrg() throws SQLException {
-//		String query = "SELECT Org_ID FROM Team WHERE ID = '" + teamID + "';";
-//		PreparedStatement ps = con.prepareStatement(query);
-//		ResultSet rs = ps.executeQuery();
-//		rs.first();
-//		int id = rs.getInt(1);
-//		rs.close();
-//		ps.close();
-//		return id;
-//	}
-//
-//	Team(int id) throws SQLException, ClassNotFoundException, IOException {
-//		super();
-//		this.teamID = id;
-//		getTeamName();
-//		setOrgID(getOrg());
-//	}
-//
-//	public String getName() {
-//		return teamName;
-//	}
-
 	private void readMembersFromDB() throws SQLException, ClassNotFoundException, IOException {
 		String query = "SELECT user FROM userteam WHERE team = '" + this.teamID + "';";
 		ResultSet rs = MariaDBClient.getInstance().runSelectQuery(query);
@@ -84,10 +39,5 @@ public class Team {
 		}
 		rs.close();
 	}
-//
-//	@Override
-//	public void finalize() throws SQLException {
-//		con.close();
-//	}
 
 }
